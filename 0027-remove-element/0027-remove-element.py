2b9edddfd7ca2e3caaaf8c4e21c230bc832caee6
Sujan KS <sujanksgowda2005@gmail.com>
2026-07-17 23:31:05 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.73%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        s, f = 0, 0
+
+        while f < len(nums):
+            if nums[f] != val:
+                nums[s] = nums[f]
+                s += 1
+            f += 1
+
+        return s
